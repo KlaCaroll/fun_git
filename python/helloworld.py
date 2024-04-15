0e5f3f5226f88c318ad9ce5012f3714ea462fa04
Caroll <caroll@aive.com>
2024-04-15 11:55:28 +0200

    [ADD] calculthe level

--- python/helloworld.py
+++ python/helloworld.py
@@ -1,4 +1,9 @@
-def helloworld():
+def helloworld(x: int):
+  # just for fun
+  square : int
+  square = x*x
+  print(f'you\'ll x{square} your level in git ... ')
+
   print('hello world')
 
-helloworld()
+helloworld(2)
